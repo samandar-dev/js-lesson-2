3ed249b2b64dfcc58bcfe0dbaa76faaf19cda188
samandar-dev <samandarbohodirov01@gmail.com>
2022-03-02 12:05:59 +0500

    addds

---
+++ main.js
@@ -0,0 +1,268 @@
+// --------------------------------------------------------------------------------------------------
+// Modulees part
+
+
+
+// 1 masala ------------------------------
+
+// let a = +prompt("-->> 1 son kirintng ")
+// let b = +prompt("-->> 2 son kitriting ")
+
+// let n = a > b ? "a kotta" : a == b ? "teng" : "b kotta"
+// console.log(n);
+
+
+
+// 2 masala -----------------------------
+
+// let a = 4;
+// let b = 9;
+// let c = (Math.sqrt(a) * (1 / 6) + Math.sqrt(b) * (1 / 3)) ** 2;
+// console.log(c);
+
+
+
+// 3 masala ------------------------------
+
+// let a = +prompt(" -->> ");
+// let b = Math.floor(a % 10 * 100);
+// let c = Math.floor(a % 100 - a % 10);
+// let d = Math.floor(a / 100);
+
+// console.log(b + c + d);
+
+
+
+// 4 masala ------------------------------
+
+// let a = 123;
+// let b = Math.floor(a % 100 / 10);
+// let c = Math.floor(a / 100);
+// let d = Math.floor(a % 10);
+
+// console.log(b * 100 + c * 10 + d);
+
+
+
+// 5 masala -----------------------------
+
+// let a = 6.3;
+// let b = 8.6;
+
+// let n = 9 * (a ** 2) * b - 27 * (a ** 2) * (b ** 2) + 15 * (b ** 2);
+// console.log(n);
+
+
+
+// 6 masala -----------------------------
+
+// let r = 3;
+// let n = 2;
+// let a = 4;
+// let b = 5;
+
+// let nn = (1 + (r / 100)) ** n / Math.sqrt((a ** 2) + (b ** 2));
+// console.log(nn);
+
+
+
+// 7 masala --------------------
+
+// let x1 = -3.9;
+// let y1 = -8.2;
+// let x2 = -1.3;
+// let y2 = -2.9;
+
+// let nn = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+// console.log(nn);
+
+
+
+// 8 masala ???????????????????????????
+
+
+
+
+
+
+// 9 masala ------------------------
+
+// let a = 0.5;
+// let x = Math.sin(a)
+// console.log(x);
+
+
+
+// 10 sasala ------------------------
+
+// let a = 0.5;
+// let x = Math.cos(a)
+// console.log(x);
+
+
+// --------------------------------------------------------------------------------------------
+
+
+// Boolean Part
+
+// 1 masala -------------------------
+
+// let a = 5;
+// let b = 5;
+
+// let n = a < b ? "B katta" : a > b ? "A katta" : "Teang";
+// console.log(n);
+
+
+
+// 2 masala -------------------------
+
+// let a = 4;
+// let n = a == 7;
+// console.log(n);
+
+
+
+// 3 masala -------------------------
+
+// let a = -3;
+// let n = a > 0;
+// console.log(n);
+
+
+// 4 masala -------------------------
+
+// let a = 3;
+// let n = a < 0;
+// console.log(n);
+
+
+// 5 masala ------------------------
+
+// let a = 7;
+// let n = a % 2 != 0;
+// console.log(n);
+
+
+// 6 masala ------------------------
+
+// let a = 4;
+// let n = a % 2 == 0;
+// console.log(n);
+
+
+// 7 masala -----------------------
+
+// let a = 4;
+// let b = 5;
+// let c = 6;
+// let n = a <= b && a <= c && b <= c;
+// console.log(n);
+
+
+// 8 masala -----------------------
+
+// let a = 6;
+// let b = 1;
+// let n = a < 0 && b < 0;
+// console.log(n);
+
+
+// 9 masala -----------------------
+
+// let a = 6;
+// let b = 1;
+// let n = a > 0 && b > 0;
+// console.log(n);
+
+
+// 10 masala -------------------
+
+// let a = 12;
+// let b = 3;
+// let n = a > 0 && a % 2 == 0 && b > 0 && b % 2 == 0;
+// console.log(n);
+
+
+// ----------------------------------------------------------------------------------------------------------------
+
+
+// Integer Part
+
+
+// 1 masala -------------------------------
+
+// let a = 1;
+// let b = 0;
+// let n = a + b;
+// let nn = a * b;
+// console.log(n);
+// console.log(nn);
+
+
+// 2 masala ----------------------------
+
+// let n = 12;
+// let natija = 2 * Math.pow((n + 3), 2)
+// console.log(natija);
+
+
+// 3 masala ----------------------------
+
+// let n = 5;
+// let natija = Math.pow(((3 + n) / 2), 2);
+// console.log(natija);
+
+
+// 4 masala ----------------------------
+
+// let n = 4;
+// let x = 2;
+// let natija = Math.pow(n, x) + Math.pow(6, x);
+// console.log(natija);
+
+
+// 5 masala ---------------------------
+
+// let a = -45;
+// let n = a < 0 && Math.abs(a);
+// console.log(n);
+
+
+// 6 masala ---------------------------
+
+// let a = 3.456;
+// console.log(Math.round(a));
+
+
+// 7 masala ----------------------------
+
+// let x = 2;
+// let y = 3;
+// let n = Math.pow(x, 4) + 5 * Math.pow(x, 2) + Math.pow(x, 3) * y;
+// console.log(n);
+
+
+// 8 masala ----------------------------
+
+// let x = 2;
+// let y = 3;
+// let n = 6 * Math.pow(x, 3) * Math.pow(y, 5) + 4 * Math.pow(x, 4) * Math.pow(y, 3) - 24 * x * y;
+// console.log(n);
+
+
+// 9 masala ---------------------------
+
+// let a = 241;
+// let b = Math.floor(a / 100);
+// let c = Math.floor(a / 10 % 10);
+// let e = Math.floor(a % 10);
+
+// console.log(b + c + e);
+// console.log(b * c * e);
+
+
+// 10 masala -------------------------
+
+// let n = ((1 + (2 / 3)) - (3 + (1 / 4)))
+// console.log(Math.abs(n));
